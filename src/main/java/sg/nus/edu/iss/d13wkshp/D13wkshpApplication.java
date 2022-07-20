3231c13612b1caef833a72c12fd4de47c7ee3087
curlynn <curlynntan@yahoo.com.sg>
2022-07-20 10:04:17 +0800

    first commit

---
+++ src/main/java/sg/nus/edu/iss/d13wkshp/D13wkshpApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.d13wkshp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D13wkshpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D13wkshpApplication.class, args);
+	}
+
+}
